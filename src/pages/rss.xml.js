12bb192154abf84ced6943973d1ff0e3ee1a5113
bywxx <reus3148@gmail.com>
2026-05-13 08:39:27 +0900

    Simplify: remove dead BASE_URL handling, fix lang to ko, drop unused placeholder images

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,7 +10,7 @@ export async function GET(context) {
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `${import.meta.env.BASE_URL.replace(/\/$/, '')}/blog/${post.id}/`,
+			link: `/blog/${post.id}/`,
 		})),
 	});
 }
